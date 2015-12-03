b0cbe22daad3cfb787a2e97a83bb5143116431a9
root <eng.khaled.93@hotmail.com>
2015-12-03 03:01:24 +0200

    Add Some Functions

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -5,17 +5,56 @@
  */
 package parser;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author root
  */
-public class Parser {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public class Parser {
+    
+    static BufferedWriter writer;
+    static String token;
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        //Scanner scanner = new Scanner();
+        //scanner.startRunning();
+        writer = new BufferedWriter(new FileWriter("parser_output.txt"));
+        program();
+        
+    }
+    
+    public void match(String x) throws IOException
+    {
+        if(token.equals(x))
+        {
+            System.out.println("x is correct");
+            writer.write(x);
+        }else{
+            System.out.println(x+" don't belong to the grammer rules");
+            System.exit(0);
+        }
+    }
+    
+    public static void program()
+    {
+        statementSequence();
+    }
+    
+    public static void statementSequence()
+    {
+        while(token!=null)
+        {
+            statement();
+        }
+    }
+    
+    public static void statement()
+    {
+        
     }
     
 }
