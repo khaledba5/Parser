cbc06450389fff4d5bd647dde8aa276ddb984458
root <eng.khaled.93@hotmail.com>
2015-12-09 22:53:33 +0200

    Finish Scanner + Parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -5,9 +5,15 @@
  */
 package parser;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,43 +24,182 @@ public class Parser {
     
     static BufferedWriter writer;
     static String token;
+    static int counter;
+    static Vector<String> tokens = new Vector();
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        //Scanner scanner = new Scanner();
-        //scanner.startRunning();
+        Scanner2.startRunning();
+        BufferedReader file = new BufferedReader(new FileReader("scanner_output.txt"));
         writer = new BufferedWriter(new FileWriter("parser_output.txt"));
+        String line = file.readLine();
+        while(line!=null)
+        {
+            String words [] = line.split("\\s+");
+            for(int i=0;i<words.length;i++)
+            {
+                tokens.add(words[i]);  
+            }
+            line = file.readLine();
+        }
+
+        
+        counter =0;
+        token = tokens.get(counter);
         program();
+        writer.write("Program Found");
+        writer.close();
         
     }
     
-    public void match(String x) throws IOException
+    public static void match(String x) 
     {
         if(token.equals(x))
         {
             System.out.println("x is correct");
-            writer.write(x);
+           
+                counter++;
+                if(tokens.size()!=counter)
+                token = tokens.get(counter);
         }else{
-            System.out.println(x+" don't belong to the grammer rules");
-            System.exit(0);
+            printError();
         }
     }
     
-    public static void program()
+    public static void program() throws IOException
     {
         statementSequence();
     }
     
-    public static void statementSequence()
+    public static void statementSequence() throws IOException
     {
-        while(token!=null)
+        statement();
+        while(token.equals(";"))
         {
+            match(token);
             statement();
+        }
+        writer.write("Statement Sequence Found");
+        writer.newLine();
+    }
+    
+    public static void statement() throws IOException
+    {
+        if(token.startsWith("if"))ifStatement();
+        else if(token.equals("repeat"))repeatStatement();
+        else if(token.equals("Identifier"))assignStatement();
+        else if(token.equals("read"))readStatement();
+        else if(token.equals("write"))writeStatement();
+        else printError();
+    }
+    
+    public static void ifStatement() throws IOException
+    {
+        match("if");
+        exp();
+        match("then");
+        statementSequence();
+        if(token.equals("else"))
+        {
+            match("else");
+            statementSequence();
+        }
+        match("end");
+        writer.write("If Statement Found");
+        writer.newLine();
+    }
+    
+    public static void repeatStatement() throws IOException
+    {
+        match("repeat");
+        statementSequence();
+        match("until");
+        exp();
+        writer.write("Repeat Statement Found");
+        writer.newLine();
+    }
+    
+    public static void assignStatement() throws IOException
+    {
+        match("Identifier");
+        match(":=");
+        exp();
+        writer.write("Assignment Statement Found");
+        writer.newLine();
+    }
+    
+    public static void readStatement() throws IOException
+    {
+        match("read");
+        match("Identifier");
+        writer.write("Read Statement Found");
+        writer.newLine();
+    }
+    
+    public static void writeStatement() throws IOException
+    {
+        match("write");
+        exp();
+        writer.write("Write Statement Found");
+        writer.newLine();
+    }
+    
+    public static void exp() throws IOException
+    {
+        simpleExp();
+        if(token.equals("<")||token.equals("="))
+        {
+            match(token);
+            simpleExp();
+            writer.write("Expression Found");
+            writer.newLine();
         }
     }
     
-    public static void statement()
+    public static void simpleExp() throws IOException
     {
-        
+        term();
+        while(token.equals("+")||token.equals("-"))
+        {
+            match(token);
+            term();
+        }
+        writer.write("Simple Expression Found");
+        writer.newLine(); 
     }
+    
+    public static void term() throws IOException
+    {
+        factor();
+        while(token.equals("*")||token.equals("/"))
+        {
+            match(token);
+            factor();
+        }
+        writer.write("Term Found");
+        writer.newLine();
+    }
+    
+    public static void factor() throws IOException
+    {
+        if(token.equals("("))
+        {
+            match("(");
+            exp();
+            match(")");
+        }else if(token.equals("number"))match(token);
+        else if(token.equals("Identifier"))match(token);
+        else printError();
+        writer.write("Factor Found");
+        writer.newLine();
+    }
+    
+    public static void printError()
+    {
+        System.out.println("Error");
+        System.exit(0);
+    }
+    
+    
+    
     
 }
---
+++ src/parser/Scanner2.java
@@ -0,0 +1,205 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author khaledba5
+ */
+public class Scanner2 {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    public static boolean isNumber(char x){
+        try
+        {
+         double d = Double.parseDouble(Character.toString(x));
+        }catch(NumberFormatException e)
+        {
+            return false;
+        }
+        return true;
+    }
+    public static boolean isLetter(char x){
+        return Character.toString(x).matches("[a-zA-Z]+");
+    }
+ 
+    
+    public static void startRunning() throws FileNotFoundException, IOException {
+        // Reading From File
+        BufferedReader file = new BufferedReader(new FileReader("tiny_sample_code.txt"));
+        String line = file.readLine();
+        
+        //Writing To File
+        BufferedWriter output = new BufferedWriter(new FileWriter("scanner_output.txt"));
+        
+
+        
+        //Array Containing Reserved Words
+        String reserved []={"if","then","else","end","repeat","until","read","write"};
+        char symbols []={'+','-','*','/','=','<','>',';'};
+        
+        while(line !=null){
+            char characters[] = line.toCharArray();
+            for(int i=0;i<characters.length;i++)
+            {
+                List state = new ArrayList();       //List Store Recognized Tokens
+                char Num = 'n';
+                char Letter = 'n';
+                if(isNumber(characters[i]))
+                {
+                    Num = 'y';      //indicates this characters is digit
+                }else if(isLetter(characters[i]))
+                {
+                    Letter ='y';    //indicates this character is letter
+                }
+                switch(characters[i])
+                {
+                    
+                    //*************Start of White Spcaes State******************
+                    case ' ':
+                        continue;
+                    //*************End of White Spaces State********************
+                        
+                      
+                        
+                        
+                        
+                    //*************Start of Comment State***********************
+                    case '{':
+                        //State Of Comment
+                        while(characters[i]!='}')i++;
+                        break;
+                    //**************End of Comment State************************
+                        
+                        
+                        
+                        
+                        
+                        
+                    //*************Start of INASSIGN State**********************
+                    case ':':
+                        i++;
+                        switch(characters[i])
+                        {
+                            case '=':
+                                output.write(" := ");
+                                break;
+                            default:
+                                i--;
+                        }
+                    break;
+                    //*************End of INASSIGN State************************
+                        
+                        
+                        
+                        
+                    default:
+                    //****************Start of INNUM State**********************
+                        if(Num=='y')
+                        {
+                            while(isNumber(characters[i]))
+                            {
+                                state.add(characters[i]);
+                                i++;
+                                if(characters.length==i)break;
+                            }
+                            i--;
+                            output.write(" number ");
+                        }
+                    //****************End of INNUM State************************
+                    
+                    
+                    
+                    
+                        
+                     //**************Start of INID State************************
+                        else if(Letter=='y')
+                        {
+                            
+                            while(isLetter(characters[i]))
+                            {
+                                state.add(characters[i]);
+                                i++;
+                                if(characters.length==i)break;
+                            }
+                            i--;
+                            String word[] = new String[state.size()];
+                            StringBuilder builder = new StringBuilder();
+                            for(int j=0;j<state.size();j++)
+                            {
+                                word[j] = state.get(j).toString();
+                                builder.append(word[j]);
+                            }
+                            String Final = builder.toString();
+                            int flag =0;
+                            for(int j=0;j<reserved.length;j++)
+                            {
+                                if(Final.equals(reserved[j]))
+                                {
+                                    flag =1;
+                                    break;
+                                }
+                            }
+                            if(flag!=1)output.write(" Identifier ");
+                            else output.write(builder.toString());
+                            continue;
+                        }
+                    //****************End of INID State*************************
+                    
+                    
+                    
+                    
+                    //****************Special Symbols***************************
+                        else
+                        {
+                            for(int j=0;j<symbols.length;j++)
+                            {
+                                if(characters[i]==symbols[j])
+                                {
+                                    output.write(characters[i]);
+                                    break;
+                                }else if(characters[i]=='(')
+                                {
+                                    int count =0;
+                                    output.write("(");
+                                    while(characters[i]!=')')
+                                    {
+                                        i++;
+                                        output.write(characters[i]);
+                                        count++;
+                                    }
+                                    output.write(")");
+                                    i = i-count;
+                                    break;
+                                }
+                            }
+                            
+                        }
+                        
+                        
+                        
+                }
+            }
+            line = file.readLine();
+            output.newLine();
+        }
+        output.close();
+    }
+    
+}
